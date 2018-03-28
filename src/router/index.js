19bdd4297117d8f8cce2a1dec057e83e3cfd2b0e
EmaEvidence <emmanuelalabi563@gmail.com>
2018-03-28 12:59:46 +0100

    fix deployment error

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
 import Dashboard from '../components/Dashbaord';
 
 Vue.use(Router);
@@ -11,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Dashboard',
-      component: Home,
+      component: Dashboard,
     },
   ],
 });
